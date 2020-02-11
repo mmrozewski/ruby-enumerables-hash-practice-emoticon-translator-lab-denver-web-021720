76e04d872c511ffbfec4a6d107bbedd3c1332cfa
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-11 03:59:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,7 @@
 require 'yaml'
 require 'pry'
 
+<<<<<<< HEAD
 
 def load_library(filepath)
   emoticons = YAML.load_file(filepath)
@@ -43,3 +44,40 @@ def get_english_meaning(filepath, emoticon)
 end
 
 
+=======
+def load_library(emoticon_file)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+
+  emoticon_library = {'get_meaning'  => {}, 'get_emoticon' => {} }
+
+  emoticons.each do |meaning, value|
+    english = value[0]
+    japanese = value[1]
+    emoticon_lib['get_meaning'][japanese] = meaning
+    emoticon_lib['get_emoticon'][english] = japanese
+    
+  end
+  
+  emoticon_library
+  
+end
+
+
+def get_japanese_emoticon(emoticon_file, emoticon)
+  emoticons = load_library(file_path)
+  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+end
+
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+end
+
+def get_english_meaning(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoji = emoticons.find {|name, faces| faces[1] == emoticon }
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[0]
+end
+>>>>>>> 30051359b3f94532ac95199702c20c537a3978d7
